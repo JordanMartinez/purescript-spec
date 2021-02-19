d7ee966082b16dcae298a892f30942dad4d55d27
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-02-19 06:33:13 -0800

    Account for fromRight changes

--- src/Test/Spec/Reporter/Tap.purs
+++ src/Test/Spec/Reporter/Tap.purs
@@ -3,13 +3,13 @@ module Test.Spec.Reporter.Tap (tapReporter) where
 import Prelude
 
 import Control.Monad.State (get, modify_)
-import Data.Either (fromRight)
+import Data.Either (fromRight')
 import Data.Maybe (Maybe(..))
 import Data.String (Pattern(Pattern), joinWith, split)
 import Data.String.Regex (regex)
 import Data.String.Regex as Regex
 import Effect.Exception as Error
-import Partial.Unsafe (unsafePartial)
+import Partial.Unsafe (unsafeCrashWith)
 import Test.Spec.Console (tellLn)
 import Test.Spec.Reporter.Base (defaultReporter)
 import Test.Spec.Result (Result(..))
@@ -50,11 +50,11 @@ tapReporter = defaultReporter 1 case _ of
 -- create a TAP-safe title
 escMsg :: String -> String
 escMsg =
-  let rex = unsafePartial $ fromRight $ regex "^" $ Regex.parseFlags "gm"
+  let rex = fromRight' (\_ -> unsafeCrashWith "Invalid regex in escMsg") $ regex "^" $ Regex.parseFlags "gm"
     in Regex.replace rex "  "
 
 -- create a TAP-safe error msg
 escTitle :: String -> String
 escTitle =
-  let rex = unsafePartial $ fromRight $ regex "#" $ Regex.parseFlags "g"
+  let rex = fromRight' (\_ -> unsafeCrashWith "Invalid regex in escTitle") $ regex "#" $ Regex.parseFlags "g"
     in Regex.replace rex ""
